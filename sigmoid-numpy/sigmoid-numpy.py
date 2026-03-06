0f0d843c0199f4434f051af9b8dc57babb4fa361
Priyanshu Singh <priyanshu3303@gmail.com>
2026-03-06 17:38:27 +0000

    Update solution for sigmoid-numpy

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -6,7 +6,7 @@ def sigmoid(x):
     """
     # Write code here
     x=np.asarray(x, dtype = float)
-    return np.where(
+    return np.where(      # handels large negative numbers
         x >= 0,
         1/ (1+np.exp(-x)),
         np.exp(x)/(1+np.exp(x))
